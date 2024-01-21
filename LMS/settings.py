1ad246150d343d59103379d2e1a6b7edc3aff9ab
utkarshbig <127543087+utkarshbig@users.noreply.github.com>
2024-01-22 02:11:16 +0530

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -134,8 +134,8 @@ LOGOUT_REDIRECT_URL = 'login'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'utkarshsingh639326@gmail.com'
-EMAIL_HOST_PASSWORD = 'clfq nnyh hnmf qgsy'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 
